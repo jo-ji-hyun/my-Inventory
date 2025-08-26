0ac91792f9097b9f8d585ae2f8bdf6b571214f4d
jo-ji-hyun <newterr@naver.com>
2025-08-26 14:33:10 +0900

    [Feat] UIMainMenu 작성 완료

--- Assets/02.Scripts/Character/CharacterData.cs
+++ Assets/02.Scripts/Character/CharacterData.cs
@@ -10,7 +10,7 @@ public class CharacterData
     public int Level { get; private set; }            // === 레벨 ===
     public int Exp { get; private set; }              // === 경험치 ===
     public string Info { get; private set; }          // === 캐릭 정보 ===
-    public int Money { get; private set; }          // === 캐릭 정보 ===
+    public int Money { get; private set; }            // === 캐릭 소지금 ===
 
     // === 칭호, 이름, 레벨, 경험치, 정보 ===
     public CharacterData(string newtitle, string newname, int newlevel, int newexp, string newinfo, int newmoney)
--- Assets/02.Scripts/UI/UIMainMenu.cs
+++ Assets/02.Scripts/UI/UIMainMenu.cs
@@ -8,7 +8,21 @@ public class UIMainMenu : MonoBehaviour
     public GameObject mainmenu;
     public GameObject status;
     public GameObject inventory;
-    public TextMeshProUGUI money;
+    [Header("Text")]
+    public TextMeshProUGUI money;           // === 소지금 ===
+    public TextMeshProUGUI title;           // === 칭호 ===
+    public TextMeshProUGUI characterName;   // === 이름 ===
+    public TextMeshProUGUI level;           // === 레벨 ===
+    public TextMeshProUGUI exp;             // === 경험치 ===
+    public TextMeshProUGUI info;            // === 정보 ===
+
+    private void Awake()
+    {
+        if(UIManager.Instance != null)
+        {
+            UIChange();
+        }
+    }
 
     // === 다른 창을 먼저 닫고 ===
     private void Closemenu()
@@ -38,8 +52,14 @@ public class UIMainMenu : MonoBehaviour
         inventory.SetActive(true);
     }
 
-    public void MoneyChange()
+    // === MainMenu의 UI들 갱신 ===
+    public void UIChange()
     {
-        money.text = string.Format("{0:N0}", UIManager.Instance.character.Money);
+        money.text = $"{UIManager.Instance.character.Money:N0}";
+        title.text = $"{UIManager.Instance.character.Title}";
+        characterName.text = $"{UIManager.Instance.character.Name}";
+        level.text = $"{UIManager.Instance.character.Level}";
+        exp.text = $"{UIManager.Instance.character.Exp} / {UIManager.Instance.character.Level * 5}"; // 일단 레벨비례 5배로 설정
+        info.text = $"{UIManager.Instance.character.Info}";
     }
 }
