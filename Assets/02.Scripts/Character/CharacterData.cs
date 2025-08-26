62cb1b52fd52df9f33980060f18f19311cf01cab
jo-ji-hyun <newterr@naver.com>
2025-08-26 12:04:00 +0900

    [Feat] 제너릭 싱글톤 추가
    +UIManager를 싱글톤화

--- Assets/02.Scripts/Character/CharacterData.cs
+++ Assets/02.Scripts/Character/CharacterData.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterData : MonoBehaviour
+[System.Serializable]
+public class CharacterData 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string title;         // === 칭호 ===
+    public string name;          // === 이름 ===
+    public int level;            // === 레벨 ===
+    public int exp;              // === 경험치 ===
+    public string info;          // === 캐릭 정보 ===
 
-    // Update is called once per frame
-    void Update()
+    // === 칭호, 이름, 레벨, 경험치, 정보 ===
+    public CharacterData(string newtitle, string newname, int newlevel, int newexp, string newinfo)
     {
-        
+        title = newtitle;
+        name = newname;
+        level = newlevel;
+        exp = newexp;
+        info = newinfo;
     }
 }
--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singleton<UIManager>
 {
+    // === UI¿¬°á ===
+    [SerializeField]
+    private UIMainMenu mainMenu;
+    [SerializeField]
+    private UIStatus status;
+    [SerializeField]
+    private UIInventory inventory;
+
+    // === ÆÄ±«ÇÏ¸é ¾ÈµÊ ===
+    protected override bool IsDestroy => false;
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
 
 }
